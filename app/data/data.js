8dbc3d172c584a6c9c7c1695ce03606aab60e71f
bgomezdh <bgomez@digitalhouse.com>
2023-06-20 09:37:44 -0300

    agregando imagens de servicios

---
+++ app/data/data.js
@@ -0,0 +1,14 @@
+module.exports = {
+    listServices:[
+        {
+            id: 1,
+            name: "test",
+            description: "",
+            galery: [{
+                id: 10,
+                path: "",
+                alternative: ""
+            }]
+        }
+    ]
+}
