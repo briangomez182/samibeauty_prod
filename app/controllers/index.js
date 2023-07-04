b39a68656d121adc7e1fd00e3993aeab5ccdd366
bgomezdh <bgomez@digitalhouse.com>
2023-07-04 09:40:30 -0300

    agregando estilos de index y servicios

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,11 @@
 const sendEmail = require('../email/index');
+const data = require('../data/data');
 
 module.exports = {
     home: (req, res) => {
-        return res.render('index')
+        let servicesExtensions = data.listServices.filter(data => data.typeService == 'Extensions Service');
+        let servicesEyebrows = data.listServices.filter(data => data.typeService == 'Eyebrows Service');
+        return res.render('index',{servicesExtensions, servicesEyebrows})
     },
     about: (req, res) => {
         let gallery = ['people-2.jpg', 'people-6-XL.jpg', 'banner.jpg']
--- app/data/data.js
+++ app/data/data.js
@@ -3,131 +3,121 @@ module.exports = {
         {
             id: 1,
             nameService: "Classic",
-            typeService: "Extensions Services",
-            description: "Classic or well-known 1x1 technique with this technique you can achieve curvature and lengthening of your natural eyelashes. Final result a natural and delicate style. Please go without makeup in the eye area.",
-            mainImg: "/img/1.avif",
+            typeService: "Extensions Service",
+            description: "Classic or well-known 1x1 technique with this technique you can achieve curvature and lengthening of your natural eyelashes and obtain a natural and delicate style as a result.",
+            descriptionIndex: "Classic or well-known 1x1 technique with this technique you can achieve curvature and lengthening of your natural eyelashes and obtain a natural and delicate style as a result.",
+            recommendations: ["Please go without makeup in the eye area."],
+            optionals: [""],
+            mainImg: "/img/services/pestanas/pestanas-c (1).jpg",
             gallery: [{
                 id: 1,
-                path: "/img/services/classic/classic (1).jpeg",
-                alternative: ""
+                path: "/img/services/pestanas/pestanas-c (1).jpg",
+                alternative: "Classic"
             },
             {
                 id: 2,
-                path: "/img/services/classic/classic (2).jpeg",
-                alternative: ""
+                path: "/img/services/pestanas/pestanas-c (2).jpg",
+                alternative: "Classic"
             },
             {
                 id: 3,
-                path: "/img/services/classic/classic (3).jpg",
-                alternative: ""
-            },
-            {
-                id: 4,
-                path: "/img/services/classic/classic (4).jpg",
-                alternative: ""
-            },
-            {
-                id: 5,
-                path: "/img/services/classic/classic (5).jpg",
-                alternative: ""
-            },
-            {
-                id: 6,
-                path: "/img/services/classic/classic (6).jpg",
-                alternative: ""
-            },
-            {
-                id: 7,
-                path: "/img/services/classic/classic (7).jpg",
-                alternative: ""
-            },
-            {
-                id: 8,
-                path: "/img/services/classic/classic (8).jpg",
-                alternative: ""
-            },
-            {
-                id: 9,
-                path: "/img/services/classic/classic (9).jpg",
-                alternative: ""
-            },
-            {
-                id: 10,
-                path: "/img/services/classic/classic (10).jpg",
-                alternative: ""
-            },
-            {
-                id: 11,
-                path: "/img/services/classic/classic (11).jpg",
-                alternative: ""
-            },
-            {
-                id: 12,
-                path: "/img/services/classic/classic (12).jpg",
-                alternative: ""
-            }]
+                path: "/img/services/pestanas/pestanas-c (3).jpg",
+                alternative: "Classic"
+            }
+        ]
         },
         {
             id: 2,
             nameService: "Hybrid",
-            typeService: "Extensions Services",
-            description: "It consists of the mixture of two techniques, the 1x1 added to a 2D in order to obtain results that continue to look natural, but that give a lot of style and a little more depth to your eyes.",
-            mainImg: "https://images.unsplash.com/photo-1577912081467-7787def464c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+            typeService: "Extensions Service",
+            description: "It consists of the mixture of two techniques, the 1x1 technique added to a 2D technique in order to obtain results that continue to look natural, but that give a lot of style and a little more depth to your eyes.",
+            descriptionIndex: "It consists of the mixture of two techniques, the 1x1 technique added to a 2D technique in order to obtain results that continue to look natural, but that give a lot of style and a little more depth to your eyes.",
+            recommendations: [],
+            optionals: [],
+            mainImg: "/img/services/pestanas/pestanas-h (1).jpeg",
             gallery: [{
-                id: 20,
-                path: "",
-                alternative: ""
-            }]
+                id: 1,
+                path: "/img/services/pestanas/pestanas-h (1).jpeg",
+                alternative: "Hybrid"
+            },
+            {
+                id: 2,
+                path: "/img/services/pestanas/pestanas-h (2).jpg",
+                alternative: "Hybrid"
+            },
+            {
+                id: 3,
+                path: "/img/services/pestanas/pestanas-h (3).jpg",
+                alternative: "Hybrid"
+            }
+        ]
         },
         {
             id: 3,
             nameService: "Deluxe",
-            typeService: "Eyebrows Services",
-            description: "It is a luxury service in which we design your eyebrows according to the natural structure of your face, we do a depilation with soft wax, and it includes cold therapy with aloe masks and drainage massages + makeup that is optional.",
-            mainImg: "test.png",
+            typeService: "Eyebrows Service",
+            description: "Service in which we design your eyebrows according to the natural structure of your face, with soft wax hair removal, includes drainage and cold therapy.",
+            descriptionIndex: "Service in which we design your eyebrows according to the natural structure of your face, with soft wax hair removal, includes drainage and cold therapy.",
+            mainImg: "/img/services/cejas/cejas-d (2).jpeg",
+            recommendations: [],
+            optionals: ['Make-up'],
             gallery: [{
-                id: 30,
-                path: "",
-                alternative: ""
+                id: 1,
+                path: "/img/services/cejas/cejas-d (1).jpg",
+                alternative: "Deluxe"
+            },
+            {
+                id: 1,
+                path: "/img/services/cejas/cejas-d (2).jpeg",
+                alternative: "Deluxe"
+            },
+            {
+                id: 1,
+                path: "/img/services/cejas/cejas-d (3).jpeg",
+                alternative: "Deluxe"
             }]
         }
         ,
         {
             id: 4,
-            nameService: "Hybrid",
-            typeService: "Eyebrows Services",
-            description: "Eyebrow waxing service design according to the structure of your face. Optional : Makeup Optional : Drainage massage + cold therapy.",
-            mainImg: "test.png",
+            nameService: "Hybrid Brows",
+            typeService: "Eyebrows Service",
+            description: "Hybrid tinting and waxing eyebrow design.",
+            descriptionIndex: "Hybrid tinting and waxing eyebrow design.",
+            mainImg: "/img/services/cejas/cejas-h (1).jpg",
+            recommendations: ['With a duration of 7 to 10 days on the skin and 4 weeks on the hair'],
+            optionals: [],
             gallery: [{
-                id: 30,
-                path: "",
-                alternative: ""
+                id: 40,
+                path: "/img/services/cejas/cejas-h (1).jpg",
+                alternative: "Hybrid"
+            },
+            {
+                id: 40,
+                path: "/img/services/cejas/cejas-h (2).jpg",
+                alternative: "Hybrid"
             }]
         },
         {
             id: 5,
-            nameService: "Deluxe",
-            typeService: "Eyebrows Services",
-            description: "It is a luxury service in which we design your eyebrows according to the natural structure of your face, we do a depilation with soft wax, and it includes cold therapy with aloe masks and drainage massages + makeup that is optional.",
-            mainImg: "test.png",
+            nameService: "Brow Lamination Deluxe",
+            typeService: "Eyebrows Service",
+            description: "Procedure that makes them look more populated and orderly lasts about 2 months. Lamination of the eyebrows consists of performing an eyebrow ironing, it is not exclusively for those who have excessively populated or rebellious eyebrows, it is for all those people who want to show off a more sophisticated image. When ironing the eyebrows, they will be combed in one direction and will look perfect, since this helps to correctly define their shape.",
+            descriptionIndex: "Procedure that makes them look more populated and orderly lasts about 2 months. Lamination of the eyebrows consists of performing an eyebrow ironing, it is not exclusively for those who have excessively populated or rebellious eyebrows, it is for all those people who want to show off a more sophisticated image...",
+            mainImg: "/img/services/cejas/cejas-lumi.jpeg",
+            recommendations: ['Includes styling and waxing'],
+            optionals: [],
             gallery: [{
                 id: 30,
-                path: "",
-                alternative: ""
-            }]
-        }
-        ,
-        {
-            id: 6,
-            nameService: "Hybrid",
-            typeService: "Eyebrows Services",
-            description: "Eyebrow waxing service design according to the structure of your face. Optional : Makeup Optional : Drainage massage + cold therapy.",
-            mainImg: "test.png",
-            gallery: [{
+                path: "/img/services/cejas/cejas-lumi.jpeg",
+                alternative: "Lamination Deluxe"
+            },
+            {
                 id: 30,
-                path: "",
-                alternative: ""
+                path: "/img/services/cejas/cejas-lumi.jpeg",
+                alternative: "Lamination Deluxe"
             }]
         }
+        
     ]
 }
