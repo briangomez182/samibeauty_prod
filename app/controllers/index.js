8806271dd0b26a93e9ae2b22c9d7771ef627288a
bgomezdh <bgomez@digitalhouse.com>
2023-07-04 18:40:33 -0300

    cambio de imagenes biografia

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,7 +8,7 @@ module.exports = {
         return res.render('index',{servicesExtensions, servicesEyebrows})
     },
     about: (req, res) => {
-        let gallery = ['people-2.jpg', 'people-6-XL.jpg', 'banner.jpg']
+        let gallery = ['people-2.jpg', 'people-6-X.JPG', 'banner.JPG']
         return res.render('about-me', {gallery})
     }
 }
--- app/data/data.js
+++ app/data/data.js
@@ -8,10 +8,10 @@ module.exports = {
             descriptionIndex: "Classic or well-known 1x1 technique with this technique you can achieve curvature and lengthening of your natural eyelashes and obtain a natural and delicate style as a result.",
             recommendations: ["Please go without makeup in the eye area."],
             optionals: [""],
-            mainImg: "/img/services/pestanas/pestanas-c (1).jpg",
+            mainImg: "/img/services/pestanas/pestanas-c1.jpg",
             gallery: [{
                 id: 1,
-                path: "/img/services/pestanas/pestanas-c (1).jpg",
+                path: "/img/services/pestanas/pestanas-c1.jpg",
                 alternative: "Classic"
             },
             {
