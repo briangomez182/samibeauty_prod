f96f3abc6624165a01ad5303bf67e1b4066d7954
bgomezdh <bgomez@digitalhouse.com>
2023-07-06 22:08:16 -0300

    clear cookie

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -32,16 +32,19 @@ module.exports = {
     },
     lang: (req, res) => {
 
-        res.cookie('langES', 'ES', {maxAge: 1000 * 60 * 20})
-
-        if (req.session.langES == undefined) {
-            /* Esta en ingles */
-            req.session.langES = true;
-
-        } else {
-            /* Esta em español */
+        if (req.cookies.langES == 'ES') {
+            res.clearCookie('langES');
             req.session.langES = undefined;
-        }
+        } else {
+            res.cookie('langES', 'ES', {maxAge: 1000 * 60 * 20})
+            if (req.session.langES == undefined) {
+                req.session.langES = true;
+    
+            } else {
+                req.session.langES = undefined;
+            }
+        }  
+        
         let path = req.query.path;
         return res.redirect(path);
     }
